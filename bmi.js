7d5a926090b6e3a415b51b6de2ec0194918a3cd3
Dominika Cygankiewicz <cdominika@github.com>
2019-01-05 23:02:16 +0100

    modal display function

--- bmi.js
+++ bmi.js
@@ -1,7 +1,21 @@
 const weight = document.getElementById("weight");
 const height = document.getElementById("height");
 const button = document.getElementById("calc");
+const buttonClose = document.getElementsByClassName("close")[0];
+const modal = document.getElementsByClassName('modal')[0];
+const mask = document.getElementsByClassName('mask')[0];
+let resultTitle = document.getElementsByTagName('h2')[0];
+let resultText = document.getElementsByClassName('results')[0];
 
+function showResult() {
+    mask.style.display = 'block';
+    modal.style.display = 'block';
+}
+
+function closeResult() {
+    mask.style.display = "none";
+    modal.style.display = "none";
+}
 
 function countBmi() {
     if (isNaN(weight.value) || isNaN(height.value)) {
@@ -10,21 +24,30 @@ function countBmi() {
         return;
     } else {
         let bmi = weight.value / (height.value * height.value);
+        showResult();
         if (bmi < 16) {
+            resultTitle.innerHTML = "Wygłodzenie";
             console.log("wygłodzenie");
         } else if (bmi >= 16 && bmi <= 16.99) {
+            resultTitle.innerHTML = "Wychudzenie";
             console.log("wychudzenie");
         } else if (bmi > 16.99 && bmi <= 18.49) {
+            resultTitle.innerHTML = "Niedowaga";
             console.log("niedowaga");
         } else if (bmi >= 18.5 && bmi <= 24.99) {
+            resultTitle.innerHTML = "Waga prawidłowa";
             console.log("waga prawidłowa");
         } else if (bmi >= 25 && bmi <= 29.99) {
+            resultTitle.innerHTML = "Nadwaga";
             console.log("nadwaga");
         } else if (bmi >= 30 && bmi <= 34.99) {
+            resultTitle.innerHTML = "I stopień otyłości";
             console.log("I stopień otyłości");
         } else if (bmi >= 35 && bmi <= 39.99) {
+            resultTitle.innerHTML = "II stopień otyłości";
             console.log("II stopień otyłości");
         } else if (bmi >= 40) {
+            resultTitle.innerHTML = "Otyłość skrajna";
             console.log("otyłość skrajna");
         }
     }
@@ -33,3 +56,4 @@ function countBmi() {
 }
 
 button.onclick = countBmi;
+buttonClose.onclick = closeResult;
